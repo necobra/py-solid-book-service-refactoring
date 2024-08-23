49685ae28a63b92c03a7ac156f4b67f6d2511cb5
necobra <necobrane105@gmail.com>
2024-08-23 11:05:38 +0300

    flake8 refactor

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 import json
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as Et
 from abc import ABC, abstractmethod
 
 
 class Book:
-    def __init__(self, title: str, content: str):
+    def __init__(self, title: str, content: str) -> None:
         self.title = title
         self.content = content
 
@@ -43,23 +43,23 @@ class PrintOutput(OutputBookHandler):
 
 class SerializeOutput(ABC):
     @abstractmethod
-    def serialize(self, book) -> str:
+    def serialize(self, book: Book) -> str:
         pass
 
 
 class JsonSerializeOutput(SerializeOutput):
-    def serialize(self, book) -> str:
+    def serialize(self, book: Book) -> str:
         return json.dumps({"title": book.title, "content": book.content})
 
 
 class XmlSerializeOutput(SerializeOutput):
-    def serialize(self, book) -> str:
-        root = ET.Element("book")
-        title = ET.SubElement(root, "title")
+    def serialize(self, book: Book) -> str:
+        root = Et.Element("book")
+        title = Et.SubElement(root, "title")
         title.text = book.title
-        content = ET.SubElement(root, "content")
+        content = Et.SubElement(root, "content")
         content.text = book.content
-        return ET.tostring(root, encoding="unicode")
+        return Et.tostring(root, encoding="unicode")
 
 
 def main(book: Book, commands: list[tuple[str, str]]) -> None | str:
